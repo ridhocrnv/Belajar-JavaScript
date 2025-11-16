5911c9d27bacda4e53345b54e981fb1ab3b2d3d5
ridhocrnv <ridhoahmadirawan281@gmail.com>
2025-11-16 22:43:24 +0800

    Info: [DONE] Belajar JavaScript Standard Library

---
+++ JAVASCRIPT STANDARD LIBRARY/Done.js
@@ -0,0 +1,14 @@
+/* 
+
+Ya, petualangan Regular Expressions (Regex) kita (Modul 1 sampai 24) telah selesai!
+
+Anda benar bahwa Regex adalah bagian penting dari JavaScript Standard Library, sama seperti 
+Array, Math, Date, dan semua metode bawaan String (seperti .match(), .replace(), 
+dan .test() yang kita pelajari).
+
+Jadi, untuk "petualangan" spesifik yang baru saja kita jalani ini, jawabannya adalah ya, sudah 
+tuntas! Selamat! 🎉
+
+*/
+
+console.log("JavaScript Standard Library: DONE")
